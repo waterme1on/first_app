ae110a31c269abb640e5c73c31240a51600a2cb0
Simon Liu <xiao.liu@spirent.com>
2013-05-20 15:17:00 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'f750df31a8ae607e3542b954f41379ae7d01beeb28d55a6f44670191add75f71854e63173d84bebf43425e2070d5cc188ecbf0ea69f41ed4b24d65511c3c5f5c'
